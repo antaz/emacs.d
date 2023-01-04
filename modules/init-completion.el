4b78db310ac3b186c6bd0ccf2fe7beb4d113ac4f
blocr <blocr@proton.me>
2023-01-04 13:13:29 +0100

    added orderless

--- modules/init-completion.el
+++ modules/init-completion.el
@@ -29,6 +29,12 @@
 
 (use-package consult)
 
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
+
 (use-package embark
   :ensure t
 
