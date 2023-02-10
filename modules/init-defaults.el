137b87ca147011f050cb64eb7fc3191d7a80a758
blocr <blocr@proton.me>
2023-02-10 19:21:11 +0100

    set fill-column

--- modules/init-defaults.el
+++ modules/init-defaults.el
@@ -1,6 +1,7 @@
 ;;;  -*- lexical-binding: t -*-
 
-(setq indent-tabs-mode nil)
+(setq-default indent-tabs-mode nil)
+(setq-default fill-column 80)
 (setq backup-directory-alist `(("." . "~/.emacs.d/backup")))
 
 (provide 'init-defaults)
